dc9ea14f45f07e2592b5749941b9de70b0ea290a
Chelovek <kadyr.ismailov.14@gmail.com>
2024-12-14 16:16:39 +0600

    create order app full

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
+from carts.models import Cart
+from orders.admin import OrderTabulareAdmin
 from users.models import User
 
 admin.site.register(User)
-
+class UserAdmin(admin.ModelAdmin):
+    inlines = [OrderTabulareAdmin]
